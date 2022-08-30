68cbedccafc28fde7d2157ab5ee471afa3f3ca27
Ryan Blecher <notryanb@gmail.com>
2022-08-30 18:40:19 -0400

    Add a tools page and return the visitors public ip

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::io::prelude::*;
 use std::net::SocketAddr;
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
+use hyper::server::conn::AddrStream;
 use hyper::service::{make_service_fn, service_fn};
 
-async fn app(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+async fn app(req: Request<Body>, addr: SocketAddr) -> Result<Response<Body>, hyper::Error> {
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/") => {
             let mut file = std::fs::File::open("./index.html")
@@ -23,6 +24,20 @@ async fn app(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
 
             Ok(Response::new(Body::from(layout)))
         },
+        (&Method::GET, "/tools") => {
+            let mut file = std::fs::File::open("./tools.html")
+                .expect("failed to find tools.html");
+            let mut layout = String::new();
+            file.read_to_string(&mut layout)
+                .expect("failed to read file");
+
+            Ok(Response::new(Body::from(layout)))
+        },
+        (&Method::GET, "/ip") => {
+            let layout = addr.to_string();
+
+            Ok(Response::new(Body::from(layout)))
+        },
         _ => {
             let mut not_found = Response::default();
             *not_found.status_mut() = StatusCode::NOT_FOUND;
@@ -34,8 +49,12 @@ async fn app(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
-    let make_service = make_service_fn(|_conn| {
-        async {  Ok::<_, hyper::Error>(service_fn(app)) }
+    let make_service = make_service_fn(move |conn: &AddrStream| {
+        let addr = conn.remote_addr();
+        async move {  
+            let addr = addr.clone();
+            Ok::<_, hyper::Error>(service_fn(move |req| app(req, addr))) 
+        }
     });
 
     let addr = SocketAddr::from(([127,0,0,1], 3333));
