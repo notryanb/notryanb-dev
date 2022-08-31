e5d7d1146883cca8758a8bae55e24de53e0269ea
Ryan Blecher <notryanb@gmail.com>
2022-08-30 20:38:34 -0400

    That was silly. I should be forwarding the ip from the upstream proxy.

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,45 @@
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use std::io::prelude::*;
 use std::net::SocketAddr;
-use hyper::{Body, Method, Request, Response, Server, StatusCode};
-use hyper::server::conn::AddrStream;
-use hyper::service::{make_service_fn, service_fn};
 
-async fn app(req: Request<Body>, addr: SocketAddr) -> Result<Response<Body>, hyper::Error> {
+async fn app(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/") => {
-            let mut file = std::fs::File::open("./index.html")
-                .expect("failed to find index.html");
+            let mut file = std::fs::File::open("./index.html").expect("failed to find index.html");
             let mut layout = String::new();
             file.read_to_string(&mut layout)
                 .expect("failed to read file");
 
             Ok(Response::new(Body::from(layout)))
-        },
+        }
         (&Method::GET, "/about") => {
-            let mut file = std::fs::File::open("./about.html")
-                .expect("failed to find about.html");
+            let mut file = std::fs::File::open("./about.html").expect("failed to find about.html");
             let mut layout = String::new();
             file.read_to_string(&mut layout)
                 .expect("failed to read file");
 
             Ok(Response::new(Body::from(layout)))
-        },
+        }
         (&Method::GET, "/tools") => {
-            let mut file = std::fs::File::open("./tools.html")
-                .expect("failed to find tools.html");
+            let mut file = std::fs::File::open("./tools.html").expect("failed to find tools.html");
             let mut layout = String::new();
             file.read_to_string(&mut layout)
                 .expect("failed to read file");
 
             Ok(Response::new(Body::from(layout)))
-        },
+        }
         (&Method::GET, "/ip") => {
-            let layout = addr.to_string();
+            let mut ip = "0.0.0.0".to_string();
+            let ip_header = req.headers().get("X-Forwarded-For");
 
-            Ok(Response::new(Body::from(layout)))
-        },
+            if let Some(header_value) = ip_header {
+                let header_value = header_value.clone();
+                ip = String::from(header_value.to_str().unwrap());
+            }
+
+            Ok(Response::new(Body::from(format!("{}\n", ip))))
+        }
         _ => {
             let mut not_found = Response::default();
             *not_found.status_mut() = StatusCode::NOT_FOUND;
@@ -48,16 +50,10 @@ async fn app(req: Request<Body>, addr: SocketAddr) -> Result<Response<Body>, hyp
 
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let make_service =
+        make_service_fn(move |_conn| async { Ok::<_, hyper::Error>(service_fn(app)) });
 
-    let make_service = make_service_fn(move |conn: &AddrStream| {
-        let addr = conn.remote_addr();
-        async move {  
-            let addr = addr.clone();
-            Ok::<_, hyper::Error>(service_fn(move |req| app(req, addr))) 
-        }
-    });
-
-    let addr = SocketAddr::from(([127,0,0,1], 3333));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3333));
     let server = Server::bind(&addr).serve(make_service);
 
     println!("Listening on https://{}", addr);
